b475e9242038145b5ff7b989c117c14c65075232
Maliqi Mario <maliqimario@gmail.com>
2022-11-30 17:48:09 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/refresh", (req, res) => {
+  console.log(req.body.refreshToken);
+  const refreshToken = req.body.refreshToken;
+
+  const spotifyApi = new SpotifyWebApi({
+    clientId: "ea7be1d9b0224c26999adf7248f9fc41",
+    clientSecret: "da9eeb82c33541d4aeec04283be9f2be",
+    redirectUri: "http://localhost:3000",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log(data.body);
+      //spotifyApi.setAccessToken(data.body.access_token);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    clientId: "ea7be1d9b0224c26999adf7248f9fc41",
+    clientSecret: "da9eeb82c33541d4aeec04283be9f2be",
+    redirectUri: "http://localhost:3000",
+  });
+
+  // .then() resolves the promise
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
